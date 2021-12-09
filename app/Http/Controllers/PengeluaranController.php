50201868dbb77745bee7b60be283333a5d609dad
syakil <syakilahmadginanjar@gmail.com>
2021-12-09 09:17:46 +0700

    pengeluaran

--- app/Http/Controllers/PengeluaranController.php
+++ app/Http/Controllers/PengeluaranController.php
@@ -13,41 +13,45 @@ use Ramsey\Uuid\Uuid;
 
 class PengeluaranController extends Controller
 {
-    public function index()
-    {
+    public function index(){
+
       $coa = Coa::all()->where('gr_head', '=', '1300000');
-    
-       return view('pengeluaran.index', compact('coa')); 
+      return view('pengeluaran.index', compact('coa')); 
 
     }
 
-    public function listData()
-    {
+    public function listData(){
     
       $pengeluaran = Pengeluaran::orderBy('id_pengeluaran', 'desc')->get();
       $no = 0;
       $data = array();
+
       foreach($pengeluaran as $list){
-         $no ++;
-         $row = array();
-         $row[] = $no;
-         $row[] = tanggal_indonesia(substr($list->created_at, 0, 10), false);
-         $row[] = $list->jenis_pengeluaran;
-         $row[] = $list->jenis_transaksi;         
-         $row[] = "Rp. ".format_uang($list->nominal);
-         $row[] = '<div class="btn-group">
-                    <a onclick="editForm('.$list->id_pengeluaran.')" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i></a>
-                    <a onclick="deleteData('.$list->id_pengeluaran.')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a></div>';
-         $data[] = $row;
+        $no ++;
+        $row = array();
+        $row[] = $no;
+        $row[] = tanggal_indonesia(substr($list->created_at, 0, 10), false);
+        $row[] = $list->jenis_pengeluaran;
+        if ($list->jenis_transaksi == 1) {
+          $row[] = "BANK";         
+        }else {
+          $row[] = "Lainnya";
+        }
+        $row[] = "Rp. ".format_uang($list->nominal);
+        $row[] = '<div class="btn-group">
+                  <a onclick="editForm('.$list->id_pengeluaran.')" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i></a>
+                  <a onclick="deleteData('.$list->id_pengeluaran.')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a></div>';
+        $data[] = $row;
       }
 
-      return Datatables::of($data)->escapeColumns([])->make(true);
+      $output = array("data" => $data);
+      return response()->json($output);
+    
     }
 
-    public function store(Request $request)
-    {
-      //$now = \Carbon\Carbon::now();
-$param_tgl = \App\ParamTgl::where('nama_param_tgl','tanggal_transaksi')->where('unit',Auth::user()->id)->first();
+    public function store(Request $request){
+
+      $param_tgl = \App\ParamTgl::where('nama_param_tgl','tanggal_transaksi')->where('unit',Auth::user()->id)->first();
       $now = $param_tgl->param_tgl;
 
       $kode=Uuid::uuid4()->getHex();
@@ -57,78 +61,79 @@ $param_tgl = \App\ParamTgl::where('nama_param_tgl','tanggal_transaksi')->where('
       $branch_coa_aktiva_user = Branch::find(Auth::user()->unit);
       $coa_aktiva_user=$branch_coa_aktiva_user->aktiva;
 
-        $pengeluaran = new Pengeluaran;
-        $pengeluaran->jenis_pengeluaran   = $request['ket'];
-$pengeluaran->jenis_pengeluaran   = $unit;
-        $pengeluaran->jenis_transaksi   = $request['trns'];        
-        $pengeluaran->nominal = $request['nominal'];
-        $pengeluaran->save();
+      $pengeluaran = new Pengeluaran;
+      $pengeluaran->jenis_pengeluaran = $request['ket'];
+      $pengeluaran->kode_transaksi = $kode_t;
+      $pengeluaran->unit = $unit;
+      $pengeluaran->jenis_transaksi = $request['trns'];        
+      $pengeluaran->nominal = $request['nominal'];
+      $pengeluaran->save();
         
-        $jurnal = new TabelTransaksi;
-         $jurnal->unit =  Auth::user()->unit; 
-         $jurnal->kode_transaksi = $kode_t;
-         $jurnal->kode_rekening = 2500000;
-         $jurnal->tanggal_transaksi = $now;
-         $jurnal->jenis_transaksi  = 'Jurnal System';
-         $jurnal->keterangan_transaksi = 'Setoran Toko ';
-         $jurnal->debet = $request['nominal'];
-         $jurnal->kredit = 0;
-         $jurnal->tanggal_posting = ' ';
-         $jurnal->keterangan_posting = '0';
-         $jurnal->id_admin = Auth::user()->id; 
-         $jurnal->save();
+      $jurnal = new TabelTransaksi;
+      $jurnal->unit =  Auth::user()->unit; 
+      $jurnal->kode_transaksi = $kode_t;
+      $jurnal->kode_rekening = 2500000;
+      $jurnal->tanggal_transaksi = $now;
+      $jurnal->jenis_transaksi  = 'Jurnal System';
+      $jurnal->keterangan_transaksi = 'Setoran Toko ';
+      $jurnal->debet = $request['nominal'];
+      $jurnal->kredit = 0;
+      $jurnal->tanggal_posting = ' ';
+      $jurnal->keterangan_posting = '0';
+      $jurnal->id_admin = Auth::user()->id; 
+      $jurnal->save();
 
-         $jurnal = new TabelTransaksi;
-         $jurnal->unit =  Auth::user()->unit; 
-         $jurnal->kode_transaksi =$kode_t;
-         $jurnal->kode_rekening = 1120000;
-         $jurnal->tanggal_transaksi = $now;
-         $jurnal->jenis_transaksi  = 'Jurnal System';
-         $jurnal->keterangan_transaksi = 'Setoran Toko ';
-         $jurnal->debet =0;
-         $jurnal->kredit = $request['nominal'];
-         $jurnal->tanggal_posting = ' ';
-         $jurnal->keterangan_posting = '0';
-         $jurnal->id_admin = Auth::user()->id; 
-         $jurnal->save();
+      $jurnal = new TabelTransaksi;
+      $jurnal->unit =  Auth::user()->unit; 
+      $jurnal->kode_transaksi =$kode_t;
+      $jurnal->kode_rekening = 1120000;
+      $jurnal->tanggal_transaksi = $now;
+      $jurnal->jenis_transaksi  = 'Jurnal System';
+      $jurnal->keterangan_transaksi = 'Setoran Toko ';
+      $jurnal->debet =0;
+      $jurnal->kredit = $request['nominal'];
+      $jurnal->tanggal_posting = ' ';
+      $jurnal->keterangan_posting = '0';
+      $jurnal->id_admin = Auth::user()->id; 
+      $jurnal->save();
 
-         $jurnal = new TabelTransaksi;
-         $jurnal->unit =  Auth::user()->unit; 
-         $jurnal->kode_transaksi = $kode_t;
-         $jurnal->kode_rekening =("1010-".$request['coa']);
-         $jurnal->tanggal_transaksi = $now;
-         $jurnal->jenis_transaksi  = 'Jurnal System';
-         $jurnal->keterangan_transaksi = 'Setoran Toko ';
-         $jurnal->debet = $request['nominal'];
-         $jurnal->kredit = 0;
-         $jurnal->tanggal_posting = ' ';
-         $jurnal->keterangan_posting = '0';
-         $jurnal->id_admin = Auth::user()->id; 
-         $jurnal->save();
+      $jurnal = new TabelTransaksi;
+      $jurnal->unit =  Auth::user()->unit; 
+      $jurnal->kode_transaksi = $kode_t;
+      $jurnal->kode_rekening =("1010-".$request['coa']);
+      $jurnal->tanggal_transaksi = $now;
+      $jurnal->jenis_transaksi  = 'Jurnal System';
+      $jurnal->keterangan_transaksi = 'Setoran Toko ';
+      $jurnal->debet = $request['nominal'];
+      $jurnal->kredit = 0;
+      $jurnal->tanggal_posting = ' ';
+      $jurnal->keterangan_posting = '0';
+      $jurnal->id_admin = Auth::user()->id; 
+      $jurnal->save();
 
-         $jurnal = new TabelTransaksi;
-         $jurnal->unit =  Auth::user()->unit; 
-         $jurnal->kode_transaksi =$kode_t;
-         $jurnal->kode_rekening = $coa_aktiva_user;
-         $jurnal->tanggal_transaksi = $now;
-         $jurnal->jenis_transaksi  = 'Jurnal System';
-         $jurnal->keterangan_transaksi = 'Setoran Toko';
-         $jurnal->debet =0;
-         $jurnal->kredit = $request['nominal'];
-         $jurnal->tanggal_posting = ' ';
-         $jurnal->keterangan_posting = '0';
-         $jurnal->id_admin = Auth::user()->id; 
-         $jurnal->save();       
+      $jurnal = new TabelTransaksi;
+      $jurnal->unit =  Auth::user()->unit; 
+      $jurnal->kode_transaksi =$kode_t;
+      $jurnal->kode_rekening = $coa_aktiva_user;
+      $jurnal->tanggal_transaksi = $now;
+      $jurnal->jenis_transaksi  = 'Jurnal System';
+      $jurnal->keterangan_transaksi = 'Setoran Toko';
+      $jurnal->debet =0;
+      $jurnal->kredit = $request['nominal'];
+      $jurnal->tanggal_posting = ' ';
+      $jurnal->keterangan_posting = '0';
+      $jurnal->id_admin = Auth::user()->id; 
+      $jurnal->save();       
         
-        return view('pengeluaran.index');
-
+      return back();
 
     }
 
-    public function edit($id)
-    {
+    public function edit($id){
+
       $pengeluaran = Pengeluaran::find($id);
       echo json_encode($pengeluaran);
+    
     }
 
     public function update(Request $request, $id)
@@ -143,6 +148,9 @@ $pengeluaran->jenis_pengeluaran   = $unit;
     public function destroy($id)
     {
         $pengeluaran = Pengeluaran::find($id);
+
+
+
         $pengeluaran->delete();
     }
 
